4c587a5f62c5e724d9c36f4e84c0403a0f743218
abad89 <abadalamenti89@gmail.com>
2021-10-06 01:39:43 -0400

    add update method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,12 @@ class UsersController < ApplicationController
         end
     end
 
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        render json: user
+    end
+
 
     private
 
