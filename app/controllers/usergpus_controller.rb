1691d1f43668c94198e18e4d5430dc510e2c337e
abad89 <abadalamenti89@gmail.com>
2021-10-05 21:03:24 -0400

    fix nested usergpus for user

--- app/controllers/usergpus_controller.rb
+++ app/controllers/usergpus_controller.rb
@@ -1,2 +1,13 @@
 class UsergpusController < ApplicationController
+
+    def index
+        if params[:user_id]
+            user=User.find(params[:user_id])
+            usergpus=user.usergpus
+            render json: usergpus, include: :gpu
+        else
+            usergpus = Usergpus.all
+            render json: usergpus
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :usergpus
   resources :users do
-    resources :gpus
+    resources :usergpus
   end
   resources :gpus
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
