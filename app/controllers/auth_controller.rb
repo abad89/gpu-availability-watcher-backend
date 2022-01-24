0a670b72542ad93e721bdbbcc7ca63a8a49d9605
abad89 <abadalamenti89@gmail.com>
2022-01-24 09:04:50 -0500

    add login methods

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,22 @@
+class AuthController < ApplicationController
+   
+    def login
+        user = User.find_by(email: params[:emal])
+        if user && user.authenticate(params[:password])
+            payload = {user_id: user.id}
+            token = encode_token(payload)
+            render json: {user: user, jwt: token, success "Authenticated"}
+        else
+            render json: {failure: "Wong username or password"}
+        end
+    end
+
+    def auto_login
+        if session_user
+            render json: session_user
+        else
+            render json: {errors: "No user logged in"}
+        end
+    end
+
+end
